6f81b2e86b1514ae2392b2ee2b5223bd6eefa583
LeonardoMartins <leohmsales@gmail.com>
2025-07-09 17:51:38 -0300

    atualização da condicional IPVA, porem puxando o ano em que estamos

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from datetime import date
+ano_atual = date.today().year
+
 class Carro:
     def __init__(self, marca, modelo, ano, cor):
         self.marca = marca
@@ -6,11 +9,16 @@ class Carro:
         self.cor = cor
 
     def apresentar(self):
+        print('---------------------------------------------')
         print(f'Marca : {self.marca}, Modelo : {self.modelo}')
         print(f'Ano : {self.ano}, Cor : {self.cor}')
-        if self.ano < 1995:
+        if ano_atual - self.ano >= 30:
             print(f'Este ano não paga mais IPVA')
 
 
 carro1 = Carro("GM","OPALA",1988,"OGW4128")
 carro1.apresentar()
+
+
+carro2 = Carro("FIAT","PUNTO",2012,"EYH6E42")
+carro2.apresentar()
